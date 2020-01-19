66a9639ad25792d1faa3e977baaf61a283bc955d
nathandorson <32528988+nathandorson@users.noreply.github.com>
2020-01-18 20:56:33 -0500

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,13 +1,15 @@
 var ws = null;
 var connected = false;
-var ship = null;
+var myId = null;
+var myShip = null;
 var enemyShip = null;
+var projectiles = [];
 
-class spaceShip
+class SpaceShip
 {
-    constructor()
+    constructor(id)
     {
-        this.id = -1;
+        this.id = id;
         this.x = 0;
         this.y = 0;
         this.angle = 0;
@@ -19,16 +21,62 @@ class spaceShip
         this.rocketAccel = 0.1;
         this.angleAccel = 0.1;
     }
+    draw()
+    {
+        ellipse(this.x,this.y,10,10);
+    }
 }
 
 var receivedActions = {
     setId: function(data)
     {
-        ship.id = data.id;
+        myId = data.id;
     },
-    entityUpdate: function(data)
+    shipUpdate: function(data)
     {
+        if(myId == null)
+        {
+            return;
+        }
+        if(data.id == myId)
+        {
+            if(myShip == null)
+            {
+                myShip = new SpaceShip(data.id);
+            }
+            myShip.x = data.x;
+            myShip.y = data.y;
+            myShip.vx = data.vx;
+            myShip.vy = data.vy;
+        }
+        else
+        {
+            if(enemyShip == null)
+            {
+                enemyShip = new SpaceShip(data.id);
+            }
+            enemyShip.x = data.x;
+            enemyShip.y = data.y;
+            enemyShip.vx = data.vx;
+            enemyShip.vy = data.vy;
+        }
         
+    },
+    projectileUpdate: function(data)
+    {
+        for(let i = 0; i < projectiles.length; i++)
+        {
+            let p = projectiles[i];
+            if(p.id == data.id)
+            {
+                p.x = data.x;
+                p.y = data.y;
+            }
+        }
+    },
+    projectileCreate: function(data)
+    {
+        projectiles.push({id: data.id, x: data.x, y: data.y, vx: data.vx, vy: data.vy})
     }
 }
 
@@ -67,7 +115,7 @@ function setup()
 
 function draw()
 {
-
+    
 }
 
 var wPress = false;
